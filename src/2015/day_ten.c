08d05e2252e0f38ef67bc6a02f920d3fe2b16bd3
John Biesnecker <jbiesnecker@fb.com>
2021-10-10 18:13:08 -0700

    Finished Day 10

--- src/2015/day_ten.c
+++ src/2015/day_ten.c
@@ -28,40 +28,89 @@ static void cleanupNodes(node* head) {
     }
 }
 
-static void debugPrintList(node* head) {
-    node* next = head;
-    while (next != NULL) {
-        printf("{%d : %d}->", next->count, next->num);
-        next = next->next;
+static node* nodeListFromString(const char* s, int len) {
+    int val = s[0] - '0';
+    node* listHead = newNode(val, 1);
+    node* listTail = listHead;
+
+    for (int i = 1; i < len; ++i) {
+        val = s[i] - '0';
+        if (listTail->num == val) {
+            ++listTail->count;
+        } else {
+            node* n = newNode(val, 1);
+            listTail->next = n;
+            listTail = n;
+        }
     }
-    puts("");
+    return listHead;
+}
+
+static node* step(node* old) {
+    // Create dummy head.
+    node newHead = {
+        .count = 0,
+        .num = 0,
+        .next = NULL,
+    };
+    node* newTail = &newHead;
+    while (old != NULL) {
+        if (old->count == newTail->num) {
+            ++newTail->count;
+        } else {
+            newTail->next = newNode(old->count, 1);
+            newTail = newTail->next;
+        }
+        if (old->num == newTail->num) {
+            ++newTail->count;
+        } else {
+            newTail->next = newNode(old->num, 1);
+            newTail = newTail->next;
+        }
+        old = old->next;
+    }
+    return newHead.next;
+}
+
+static int getLength(node* head) {
+    int total = 0;
+    while (head != NULL) {
+        total += head->count;
+        head = head->next;
+    }
+    return total;
+}
+
+static int runGameAndCleanupNodes(node* head, int rounds) {
+    for (int i = 0; i < rounds; ++i) {
+        node* newHead = step(head);
+        cleanupNodes(head);
+        head = newHead;
+    }
+
+    int solution = getLength(head);
+    cleanupNodes(head);
+    return solution;
 }
 
 FUNCTION_DEFN_FOR_YDS(2015, ten, a) {
     char buffer[100] = {0};
     fgets(buffer, sizeof(buffer), fp);
     int len = stripNewline(buffer);
-    printf("%s\n", buffer);
 
-    int val = buffer[0] - '0';
-    node* oldListHead = newNode(val, 1);
-    node* oldListTail = oldListHead;
+    node* list = nodeListFromString(buffer, len);
 
-    for (int i = 1; i < len; ++i) {
-        val = buffer[i] - '0';
-        if (oldListTail->num == val) {
-            ++oldListTail->count;
-        } else {
-            node* n = newNode(val, 1);
-            oldListTail->next = n;
-            oldListTail = n;
-        }
-    }
-
-    debugPrintList(oldListHead);
-    cleanupNodes(oldListHead);
+    int solution = runGameAndCleanupNodes(list, 40);
+    printf("%d\n", solution);
 }
 
 FUNCTION_DEFN_FOR_YDS(2015, ten, b) {
-    UNUSED_ARG(fp);
+    char buffer[100] = {0};
+    fgets(buffer, sizeof(buffer), fp);
+    int len = stripNewline(buffer);
+
+    node* list = nodeListFromString(buffer, len);
+
+    int solution = runGameAndCleanupNodes(list, 50);
+    printf("%d\n", solution);
 }
